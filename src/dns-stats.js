638bc50a7eaffdb219868ca360905b22046685ca
v-yelina <heytridvaodin@gmail.com>
2022-04-29 22:45:50 +0200

    feat: finish dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let domainsCount = {};
+  for (let domain of domains) {
+    let domainArr = domain.split(".").reverse();
+    let domainStr = ".";
+    for (let i = 0; i < domainArr.length; i++) {
+      domainStr += domainArr[i];
+      if (!domainsCount.hasOwnProperty(domainStr)) {
+        domainsCount[domainStr] = 1;
+      } else {
+        domainsCount[domainStr] += 1;
+      }
+      domainStr += ".";
+    }
+  }
+  return domainsCount;
 }
 
+// getDNSStats(["code.yandex.ru", "music.yandex.ru", "yandex.ru"]);
+
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
